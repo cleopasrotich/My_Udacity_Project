a9122e99c46d3b0196342abf3bb6278c829771ef
cleopasrotich <cleopasrotich81@gmail.com>
2018-06-16 22:54:50 +0300

    My_Udacity_Project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Pixel Art Maker Project
+
+***my project for the google Africa scholarship challenge***
+
+ ![example](art.png)
+
+ ### Instructions for using the pixel canvas:
+
+ 1. Choose the grid height and width which you want to work with.
+ 1. Then hit the submit button.
+ 1. Choose the desired color from the color picker.
+ 1. Enjoy the art work.
---
+++ designs.js
@@ -0,0 +1,33 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+$(function(){
+  let inputHeight = $("#inputHeight");
+  let inputWidth = $("#inputWidth");
+  let table = $("#pixelCanvas");
+
+  function makeGrid() {
+    let grid = "";
+    for(var r = 0; r < inputHeight.val(); r++){
+      let trId = r.toString();
+      grid += "<tr id="+ trId +">";
+        for(var c = 0; c < inputWidth.val(); c++){
+          let tdId = trId + c.toString();
+          grid += "<td id="+ tdId +"></td>"
+        };
+      grid += "</tr>";
+    };
+    return grid;
+  };
+
+  $("#submit").on("click",function(){
+    table.append(makeGrid());
+    squares = table.find("td");
+    squares.on("click", function(){
+      let color = $("#colorPicker").val();
+      $(this).css("background-color", color);
+    });
+  });
+
+});
