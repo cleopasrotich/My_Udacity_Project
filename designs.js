7edbf993275e848806138cc2ab8ef4409d04d915
cleopasrotich <cleopasrotich81@gmail.com>
2018-06-30 20:30:38 +0300

    feat: added mouse hover effects

--- designs.js
+++ designs.js
@@ -1,37 +1,42 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
 $(function(){
-  let inputHeight = $("#inputHeight");
-  let inputWidth = $("#inputWidth");
-  let table = $("#pixelCanvas");
+    let inputHeight = $("#inputHeight");
+    let inputWidth = $("#inputWidth");
+    let table = $("#pixelCanvas");
 
-  function makeGrid() {
-    let grid = "";
-    for(var r = 0; r < inputHeight.val(); r++){
-      let trId = r.toString();
-      grid += "<tr id="+ trId +">";
-        for(var c = 0; c < inputWidth.val(); c++){
-          let tdId = trId + c.toString();
-          grid += "<td id="+ tdId +"></td>"
+    function makeGrid() {
+        let grid = "";
+        for(let r = 0; r < inputHeight.val(); r++){
+            let trId = r.toString();
+            grid += "<tr id="+ trId +">";
+                for(let c = 0; c < inputWidth.val(); c++){
+                    let tdId = trId + c.toString();
+                    grid += "<td id="+ tdId +"></td>"
+                }
+            grid += "</tr>";
         }
-      grid += "</tr>";
+        return grid;
     }
-    return grid;
-  }
 
-  $("#submit").on("click",function(){
-    $(".canvas-title").remove();
-    table.append(makeGrid());
-    squares = table.find("td");
-    squares.on("click", function(){
-      let color = $("#colorPicker").val();
-      $(this).css("background-color", color);
+    // This is where the table is created
+    $("#submit").on("click",function(){
+        $(".canvas-title").remove();
+        table.append(makeGrid());
+        squares = table.find("td");
+
+        // On hover show the selected color
+        squares
+        .click(function(){
+            let color = $("#colorPicker").val();
+            $(this)
+            .css("background-color", color)
+            .unbind("mouseleave mouseenter");            
+        })        
+        .mouseenter(function(){
+            let color = $("#colorPicker").val();
+            $(this).css("background-color", color);
+        })
+        .mouseleave(function(){
+            $(this).css("background-color", "");
+        });
     });
-  });
-
-
-
-
 });
